9182d88415c0f02c00b2f9bfe58b219616bedcb9
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-11-28 20:05:14 -0300

    Projeto criado!

---
+++ src/test/java/io/github/GuilhermeHNS/controleFinanceiroApi/ControleFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.GuilhermeHNS.controleFinanceiroApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
